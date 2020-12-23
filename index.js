7b7e2f29dcf4720f006bc548a81ece8258b33a11
Angel Baro <angelbaro44@gmail.com>
2020-12-22 21:14:53 -0500

    More test Cases added

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ function persistence(num) {
       }
 
 
-
+//    Test Cases:
 
       persistence(77) //expected result 4
       persistence(4) //expected result 0
       persistence(69) //expected result 3
-    
+      persistence(999) //expected result 4
+      persistence(456901) //expected result 1
+      persistence(1000000000) //expected result 1
+      persistence(900383722382700023421) //expected result 1
 
 
