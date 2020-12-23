0e4ec0a8c38723888f7c4e9baef219f8d3963eee
Angel Baro <angelbaro44@gmail.com>
2020-12-22 21:13:31 -0500

    kata finished

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function persistence(num) {
+      let persist = 0;
+      let arr =  num.toString().split('');
+         while (arr.length > 1){
+        persist++
+        arr = arr.reduce((x,y)=>((x*y))).toString().split('');   
+      }
+      console.log(persist) 
+      return persist
+      }
+
+
+
+
 
